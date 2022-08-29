f19fe0c1e6c9ff149009af09a28075ff0adbfb60
ravilella1234 <masters.selenium@gmail.com>
2022-08-29 08:55:16 +0530

    Switching into frames  & window types(tab, new window, incognito ))

---
+++ August2022_8AM_SeleniumBatch/src/test/java/switches/SwitchWindow1.java
@@ -0,0 +1,26 @@
+package switches;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SwitchWindow1 
+{
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		
+		//driver.switchTo().newWindow(WindowType.TAB);
+		driver.switchTo().newWindow(WindowType.WINDOW);
+		driver.get("https://www.amazon.in");
+		System.out.println(driver.getTitle());
+	}
+
+}
