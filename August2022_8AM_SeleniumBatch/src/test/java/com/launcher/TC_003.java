6a34fe0517595f5ad3234e2c4460597f25203f93
ravilella1234 <masters.selenium@gmail.com>
2022-09-02 08:46:29 +0530

    Log4j

--- August2022_8AM_SeleniumBatch/src/test/java/com/launcher/TC_003.java
+++ August2022_8AM_SeleniumBatch/src/test/java/com/launcher/TC_003.java
@@ -1,18 +1,31 @@
 package com.launcher;
 
+import org.apache.log4j.Logger;
+
 public class TC_003 extends BaseTest
 {
-
+	private static final Logger log = Logger.getLogger(TC_003.class);
+	
 	public static void main(String[] args) throws Exception 
 	{
 		
 		init();
-		launch("chromebrowser");
-		navigateUrl("amazonurl");
-		selectOption("amazondropbox_id","Books");
-		typeText("amazonsearchtextbox_name","Harry Potter");
-		clickElement("amazonsearchbutton_xpath");
+		log.info("Init the Properties files...");
 		
+		launch("chromebrowser");
+		log.info("Opening the Browser :" + p.getProperty("chromebrowser"));
+		
+		navigateUrl("amazonurl");
+		log.info("Navigating to url : " + childProp.getProperty("amazonurl"));
+		
+		selectOption("amazondropbox_id","Books");
+		log.info("Selecting the option Books By using locator : " + orProp.getProperty("amazondropbox_id"));
+		
+		typeText("amazonsearchtextbox_name","Harry Potter");
+		log.info("Entering the text Harry Potter By using locator : " + orProp.getProperty("amazonsearchtextbox_name"));
+		
+		clickElement("amazonsearchbutton_xpath");
+		log.info("Clicked on element by using locator : " +  orProp.getProperty("amazonsearchbutton_xpath"));
 			
 		
 		//WebElement loc = driver.findElement(By.id("searchDropdownBox"));
