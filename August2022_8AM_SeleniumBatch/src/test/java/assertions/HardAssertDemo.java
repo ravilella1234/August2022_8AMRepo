2210e30da09f307b431558ee7ffd2515f2c0971e
ravilella1234 <masters.selenium@gmail.com>
2022-09-15 08:43:22 +0530

    Soft & Hard Asserts

---
+++ August2022_8AM_SeleniumBatch/src/test/java/assertions/HardAssertDemo.java
@@ -0,0 +1,33 @@
+package assertions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HardAssertDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in");
+		
+		String actualLink = driver.findElement(By.linkText("Customer Service")).getText();
+		System.out.println(actualLink);
+		
+		String expectedLink = "Customer Serv";
+		System.out.println(expectedLink);
+		
+		//Hard Assert
+		//Assert.assertEquals(actualLink, expectedLink);
+		Assert.assertTrue(actualLink.equals(expectedLink), "Both liks are not equal...");
+	
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("sony");
+	}
+
+}
---
+++ August2022_8AM_SeleniumBatch/src/test/java/assertions/SoftAssertDemo.java
@@ -0,0 +1,44 @@
+package assertions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SoftAssertDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in");
+		
+		String actualLink = driver.findElement(By.linkText("Customer Service")).getText();
+		System.out.println(actualLink);
+		
+		String expectedLink = "Customer Serv";
+		System.out.println(expectedLink);
+		
+		SoftAssert s = new SoftAssert();
+		//s.assertEquals(actualLink, expectedLink);
+		s.assertTrue(actualLink.equals(expectedLink), "Both links are not equal...");
+		
+		s.assertTrue(false, "err1");
+		
+		s.assertTrue(false, "err2");
+		
+		s.assertTrue(true, "err3");
+		
+		s.assertTrue(false, "err4");
+		
+		
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("sony");
+		
+		s.assertAll();
+	}
+
+}
