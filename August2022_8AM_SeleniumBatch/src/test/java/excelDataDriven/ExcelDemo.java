77cb0573d58eab2811aec5d09869758af5da16aa
ravilella1234 <masters.selenium@gmail.com>
2022-09-22 08:43:24 +0530

    ExcelDataDriven Day-3

---
+++ August2022_8AM_SeleniumBatch/src/test/java/excelDataDriven/ExcelDemo.java
@@ -0,0 +1,15 @@
+package excelDataDriven;
+
+public class ExcelDemo 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		ExcelAPI e = new ExcelAPI("C:\\Users\\ravi\\Desktop\\testdata1.xlsx");
+		
+		System.out.println(e.getCellData("login", 0, 1));
+		System.out.println(e.getCellData("login", "Password", 1));
+		e.setCellData("login", 4, 1, "Failed");
+	}
+
+}
